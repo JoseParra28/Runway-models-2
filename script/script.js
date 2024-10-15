b5dbc25623d8724195220adced8464d2c829fdc5
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-10-15 18:15:22 +0100

    cursor
    added custom cursor divs and style

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
-console.log("hello")
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener('mousemove', moveCursor);
+
+function moveCursor(e){
+    let x = e.ClientX;
+    let y = e.ClientY;
+
+    console.log(x, y)
+}
+
