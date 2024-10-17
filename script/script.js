cc2204a8718f3b78a3459ca866efcbca6e649dd8
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-10-17 16:44:15 +0100

    dropdown
    added dropdown js functionality

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function hideMenu(){
+    const dropdown = document.querySelector('.dropdown-div')
+    dropdown.style.display = 'none'
+}
+function showMenu(){
+    const dropdown = document.querySelector('.dropdown-div')
+    dropdown.style.display = 'block'
+}
+
