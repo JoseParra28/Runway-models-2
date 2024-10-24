8af75d75c0cb1f145d4226a897ead1c7d93b74f6
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-10-24 16:20:54 +0100

    email
    added send email functionality wit MailJs

--- script/script.js
+++ script/script.js
@@ -7,3 +7,14 @@ function showMenu(){
     dropdown.style.display = 'block'
 }
 
+
+function sendMail(){
+    let parms = {
+        name : document.getElementById("fname").value,
+        email : document.getElementById("email").value,
+        lname : document.getElementById("lname").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_lgmsn2k","template_trumjrn",parms).then(alert("Email has been sent")) ;
+}
+
